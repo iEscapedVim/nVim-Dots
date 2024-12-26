efd5f72511f2eb83b5eeb35794a262f3981baa12
iEscapedVim <mail>
2024-12-26 10:34:31 +0500

    my nvim dots

---
+++ lua/vim-options.lua
@@ -0,0 +1,6 @@
+-- How many spaces the Tab have? There's actually Three
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
+vim.g.mapleader = " "
